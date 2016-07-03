d16237f851f3bd4349ef5c640d53ce5e6bf9b507
王国桥 <wfdwgq2014@outlook.com>
2016-07-03 15:04:33 +0800

    day2-end

---
+++ day2/Nourriture.sh
@@ -0,0 +1,6 @@
+# centos 7.2 64bit
+yum -y install npm mysql mongodb
+npm install -g n express express-session express-router cookie-parser mysql ejs multiparty
+n stable
+# upload Server.js
+node Server.js
