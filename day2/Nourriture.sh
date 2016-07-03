13aff7fb3a96b0ea9f73e5a7975365f75d372062
王国桥 <wfdwgq2014@outlook.com>
2016-07-03 20:25:48 +0800

    day2-end2

--- day2/Nourriture.sh
+++ day2/Nourriture.sh
@@ -2,5 +2,6 @@
 yum -y install npm mysql mongodb
 npm install -g n express express-session express-router cookie-parser mysql ejs multiparty
 n stable
-# upload Server.js
-node Server.js
+mkdir /home/www/Nourriture
+cp Server.js /home/www/Nourriture/Server.js
+node /home/www/Nourriture/Server.js
